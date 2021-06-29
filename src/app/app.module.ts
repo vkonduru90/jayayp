71b32b9f4449f71e56d1459b01553855ad034e5f
jaya <jayav07@gmail.com>
2021-06-29 18:28:31 +0530

    logincomponent created

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+ }
